a93356418a4273cb9d379dfe80ab2fc55db556db
VikasKrSrivastava <110340681+VikasKrSrivastava@users.noreply.github.com>
2022-07-31 20:24:03 +0530

    Created program to check whether number is prime

---
+++ isPrime.js
@@ -0,0 +1,19 @@
+// Find a given no. is prime or not
+
+let num = 53;
+let counter = 2;
+isPrime = true;
+
+while (counter < num / 2) {
+  if (num % counter == 0) {
+    console.log(counter);
+    isPrime = false;
+  }
+  counter++;
+}
+
+if (isPrime) {
+  console.log(`The number is prime`);
+} else {
+  console.log(`The number is composite`);
+}
